a03aa41ef7e2746fecd9404c3fbe6bcac251a5ed
Vyas <vyas.racher@gmail.com>
2024-11-14 16:36:23 -0800

    shifted examplecompnent.js

---
+++ frontend/src/ExampleComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+  return (
+    <div>
+      <h2>This is the Example Component</h2>
+      <p>Here is some example content that will be shown on the /example route.</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
