2de27ec04eb665c194af93b9b64a9e31ba8e8dee
Abeni Liu <liuabeni@gmail.com>
2024-11-21 00:39:04 -0800

    Change login button

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.png';
 import { Link } from 'react-router-dom';  // Corrected import for Link
-import './Home.css'; // Correct import for Home.css
+import './Home.css'; 
 
 function Home() {
     return (
