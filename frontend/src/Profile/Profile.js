af8dbbc055fab9b3e6baf59a169dccecdfe90d51
Abeni Liu <liuabeni@gmail.com>
2025-02-25 20:36:13 -0800

    edit profile page

--- frontend/src/Profile/Profile.js
+++ frontend/src/Profile/Profile.js
@@ -130,12 +130,13 @@ const Profile = ({ onLogout }) => {
                     </div>
 
                     <div className="user-info-grid">
-                        <div className="info-card"><h3>First Name</h3><p>{userData.first_name}</p></div>
-                        <div className="info-card"><h3>Last Name</h3><p>{userData.last_name}</p></div>
+                        <div className="info-card"><h3>Name</h3><p>{userData.first_name + " " + userData.last_name}</p></div>
                         <div className="info-card"><h3>Email</h3><p>{userData.email}</p></div>
+                        <div className="info-card"><h3>Device ID</h3><p>{}</p></div>
                         <div className="info-card"><h3>Date of Birth</h3><p>{userData.date_of_birth}</p></div>
                         <div className="info-card"><h3>Age</h3><p>{calculateAge(userData.date_of_birth)}</p></div>
                         <div className="info-card"><h3>Join Date</h3><p>{userData.date_joined}</p></div>
+
                     </div>
                 </div>
             )}
