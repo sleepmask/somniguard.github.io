8ac0c2dde089c5a83a30f65a3ab85f43cf26e11b
Jason Liu <111265664+liug26@users.noreply.github.com>
2025-03-26 20:02:29 +0800

    Configured domain name & HTTPS

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const serverURL = 'http://54.183.21.65:8000/';
+export const serverURL = 'https://sleep-ec2.guojasonliu.com:8001/';
 
 // Create an axios instance with your Django backend URL
 const API = axios.create({
