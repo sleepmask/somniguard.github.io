f6a70e81d00a2f7007f41f16a96ce90935ea4a3f
Abeni Liu <liuabeni@gmail.com>
2025-04-08 22:23:45 -0700

    fix App.js uselocation function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+
 import Home from './Home/Home';
 import AboutUs from './AboutUs/AboutUs';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
 import Profile from './Profile/Profile';
-import './App.css';
 import HeartRate from './Biometrics/HeartRate';
 import Movement from './Biometrics/Movement';
 import OxygenSaturation from './Biometrics/OxygenSaturation';
 import GettingStarted from './AboutUs/GettingStarted';
-import { useLocation } from 'react-router-dom';
+
+import './App.css';
+
+function AppRoutes({ isAuthenticated, handleLogin, handleLogout, checkAuth }) {
+    const location = useLocation();
+
+    useEffect(() => {
+        checkAuth();
+    }, [location.pathname]);
+
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/getting-started" element={<GettingStarted />} />
+            <Route path="/login" element={isAuthenticated ? <Navigate to="/profile" /> : <Login onLogin={handleLogin} />} />
+            <Route path="/signup" element={isAuthenticated ? <Navigate to="/login" /> : <Signup />} />
+            <Route path="/profile" element={isAuthenticated ? <Profile onLogout={handleLogout} /> : <Navigate to="/login" />} />
+            <Route path="/heart-rate" element={isAuthenticated ? <HeartRate /> : <Navigate to="/login" />} />
+            <Route path="/movement" element={isAuthenticated ? <Movement /> : <Navigate to="/login" />} />
+            <Route path="/oxygen-saturation" element={isAuthenticated ? <OxygenSaturation /> : <Navigate to="/login" />} />
+        </Routes>
+    );
+}
 
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -19,10 +42,6 @@ function App() {
         const token = localStorage.getItem('access_token');
         setIsAuthenticated(!!token);
     };
-
-    useEffect(() => {
-        checkAuth();
-    }, []);
 
     const handleLogin = () => {
         checkAuth();
@@ -35,25 +54,14 @@ function App() {
         checkAuth();
     };
 
-    const location = useLocation();
-
-    useEffect(() => {
-        checkAuth();
-    }, [location.pathname]);
-
     return (
         <Router basename="/somniguard.github.io">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about-us" element={<AboutUs />} />
-                <Route path="/getting-started" element={<GettingStarted />} />
-                <Route path="/login" element={isAuthenticated ? <Navigate to="/profile" /> : <Login onLogin={handleLogin} />} />
-                <Route path="/signup" element={isAuthenticated ? <Navigate to="/login" /> : <Signup />} />
-                <Route path="/profile" element={isAuthenticated ? <Profile onLogout={handleLogout} /> : <Navigate to="/login" />} />
-                <Route path="/heart-rate" element={isAuthenticated ? <HeartRate /> : <Navigate to="/" />} />
-                <Route path="/movement" element={isAuthenticated ? <Movement /> : <Navigate to="/" />} />
-                <Route path="/oxygen-saturation" element={isAuthenticated ? <OxygenSaturation /> : <Navigate to="/" />} />
-            </Routes>
+            <AppRoutes
+                isAuthenticated={isAuthenticated}
+                handleLogin={handleLogin}
+                handleLogout={handleLogout}
+                checkAuth={checkAuth}
+            />
         </Router>
     );
 }
